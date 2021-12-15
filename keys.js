3313ac7e7e4ab33bd0f715b2403902dc74a8b3c2
xameryn <maximilianramann@gmail.com>
2021-12-15 13:29:12 -0800

    Delete keys.js

--- keys.js
+++
@@ -1,6 +0,0 @@
-var discordKey = 'ODk2OTM0NjI1Mzg4MTM4NTI3.YWOVdg.gffqm3fD04KSvRGvq3puIwHmd54';
-var guildKey = '730691398768263198';
-var prefixKey = '`';
-
-
-export { discordKey, guildKey, prefixKey };
