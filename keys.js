2e5d06b3cf4f9770f8ab9b4cd0b535d8ae09e6c9
xameryn <maximilianramann@gmail.com>
2021-12-15 13:25:35 -0800

    Configured .gitignore

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
-var discordKey = 'DISCORDKEY';
-var guildKey = 'GUILDKEY';
-var prefixKey = 'COMMAND PREFIX';
+var discordKey = 'ODk2OTM0NjI1Mzg4MTM4NTI3.YWOVdg.gffqm3fD04KSvRGvq3puIwHmd54';
+var guildKey = '730691398768263198';
+var prefixKey = '`';
 
 
 export { discordKey, guildKey, prefixKey };
